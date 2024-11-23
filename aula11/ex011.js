7d8fbbe876766c9f53126efd785259a5444a700a
Arthur Henrique Sena Silveira <arthurhsesi@gmail.com>
2024-11-23 19:28:47 -0300

    Condições parte 1 finalizado

---
+++ aula11/ex011.js
@@ -0,0 +1,7 @@
+var vel = 65
+console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+if (vel > 60) { //Condição simples!
+    console.log(`Você ultrapassou a velocidade permitida (60Km/h). MULTADO!!!`)
+}
+
+console.log(`Dirija sempre com o cinto de segurança`)
