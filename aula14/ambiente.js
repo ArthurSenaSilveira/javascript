e51a5e12bc81f49e02f1bbe054d01379c2fd0e37
Arthur Henrique Sena Silveira <arthurhsesi@gmail.com>
2024-11-25 17:22:41 -0300

    testando o uso do for

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,8 +1,16 @@
+numero = 10
+for (var a = 1; a <= 10; a++) {
+  console.log(`${a} x ${numero} = ${a * numero}`)
+}
+
+
+/*
 console.log('Vai começar')
 for (var c = 1; c <= 4; c++) {
   console.log(`Passo ${c}`)
 }
 console.log('Fim!')
+*/
 /*
 var c = 1 
 while (c <= 10) {
