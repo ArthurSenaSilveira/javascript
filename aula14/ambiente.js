39fdbbd8210fb2e0f962253cdd8cd42946104d74
Arthur Henrique Sena Silveira <arthurhsesi@gmail.com>
2024-11-25 14:54:04 -0300

    aula14 iniciada e finalizada

---
+++ aula14/ambiente.js
@@ -0,0 +1,12 @@
+console.log('Vai começar')
+for (var c = 1; c <= 4; c++) {
+  console.log(`Passo ${c}`)
+}
+console.log('Fim!')
+/*
+var c = 1 
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+    */
