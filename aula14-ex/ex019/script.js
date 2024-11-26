1d9141221fbfbb7ced6f71d535bea32377ce2221
Arthur Henrique Sena Silveira <arthurhsesi@gmail.com>
2024-11-26 01:15:40 -0300

    Atualização do exercício 1 da aula 14 finalizada

--- aula14-ex/ex019/script.js
+++ aula14-ex/ex019/script.js
@@ -1,25 +1,38 @@
 function clicar() {
-  var inicio = window.document.querySelector('input#inicio').value
+  let inicio = document.querySelector('#inicio')
 
-  var fim = document.querySelector('input#fim').value
+  let fim = document.querySelector('#fim')
 
-  var passo = document.querySelector('input#passo').value
+  let passo = document.querySelector('#passo')
 
-  var res = document.querySelector('input#res')
+  let res = document.querySelector('#res')
 
-  //inicio = Number(inicio)
-
-  //fim = Number(fim)
-
-  //passo = Number(passo)
-
-  if (isNaN(inicio) || isNaN(fim) || isNaN(passo) || passo <= 0) {
-    alert('[ERRO], Por Favor, Insira Números válidos.')
+  if (
+    inicio.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
+  ) {
+    res.innerHTML = 'Imposivel conta!'
+    //alert('[ERRO], Faltam dados!')
   } else {
-    res.innerHTML = 'Contando'
-    var i = Number(inicio.value)
-    var f = Number(fim.value)
-    var p = Number(passo.value)
-    if (i < f) {}
+    res.innerHTML = 'Contando: <br>'
+    let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    if (p <= 0) {
+      alert('Passo inválido! Considerando PASSO 1')
+      p = 1
+    }
+    if (i < f) {
+      for (let c = i; c <= f; c += p) {
+      res.innerHTML += `${c} \u{1F449}`
+    }
+    } else {
+      for ( let c = i; c >= f; c -= p) {
+        res.innerHTML += `${c} \u{1F449}`
+      }
+      
+    }
+    res.innerHTML += `\u{1F3C1}`
   }
 }
