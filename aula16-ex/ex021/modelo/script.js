bece6f1caa799bb5e5fc4f3f7f3d94a6a192ab93
Arthur Henrique Sena Silveira <arthurhsesi@gmail.com>
2024-11-29 14:09:00 -0300

    ultimo exercício do curso de JavaScript finalizado

--- aula16-ex/ex021/modelo/script.js
+++ aula16-ex/ex021/modelo/script.js
@@ -1,46 +1,49 @@
 let numero = []
-let quantidade = 0  // Quantidade de números inseridos
-let soma = 0        // Soma de todos os números
-let maior = 0       // Maior valor inserido
-let menor = 100     // Menor valor inserido (inicializado com 100, já que o número vai estar entre 1 e 100)
-
+let quantidade = 0 // Quantidade de números inseridos
+let soma = 0 // Soma de todos os números
+let maior = 0 // Maior valor inserido
+let menor = 100 // Menor valor inserido (inicializado com 100, já que o número vai estar entre 1 e 100)
+let res = document.querySelector('div#res')
 
 let valores = document.querySelector('select#valores')
 function clicar() {
-    let num = parseInt(document.querySelector('input#numero').value)
-    
+  let num = parseInt(document.querySelector('input#numero').value)
 
-    if (isNaN(num) || num < 1 || num > 100 || numero.includes(num)) {
-    window.alert(`[ERRO!] Este número não segue os critérios ou ja foi adicionado uma vez ou nenhum número foi inserido.`)
+  if (isNaN(num) || num < 1 || num > 100 || numero.includes(num)) {
+    window.alert(
+      `[ERRO!] Este número não segue os critérios ou ja foi adicionado uma vez ou nenhum número foi inserido.`
+    )
     return
+  }
+
+  numero.push(num)
+
+  quantidade++
+
+  soma += num
+
+  if (num > maior) {
+    maior = num
+  }
+
+  if (num < menor) {
+    menor = num
+  }
+
+  let option = document.createElement('option')
+  option.text = `Valor ${num} adicionado.\n`
+  valores.add(option) + '\n'
+
+  document.querySelector('input#numero').value = ''
 }
 
-
-    numero.push(num)
-
-    quantidade++
-
-    soma += num
-
-    if (num > maior) {
-        maior = num
-    }
-
-    if (num < menor) {
-        menor = num
-    }
-
+function resultado() {
+  if (numero.length == 0) {
+    window.alert('Adicione valores antes de finalizar')
+  } else {
     
-    let option = document.createElement('option');
-    option.text = `Valor ${num} adicionado.\n`;
-    valores.add(option) + "\n";
-
-    document.querySelector('input#numero').value = "";
-}
-
-    function resultado() {
     media = soma / quantidade
-    let resDiv = document.querySelector('#res');
+    let resDiv = document.querySelector('#res')
     resDiv.innerHTML = `
     <p>Ao todo, temos ${quantidade} números cadastrados.</p>
     <p>O maior valor informado foi ${maior}.</p>
@@ -48,6 +51,5 @@ function clicar() {
     <p>Somando todos os valores, temos ${soma}.</p>
     <p>A média dos valores digitados é ${media.toFixed(2)}.</p>
 `
-        
-    
+  }
 }
